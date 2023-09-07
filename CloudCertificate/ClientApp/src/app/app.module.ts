e9ec77524cfcf32857550072b9b0031d9d69f5e5
Andreas Dragoman <andreas.dragoman@ness.com>
2023-09-07 18:41:12 +0300

    add notif module to root

--- CloudCertificate/ClientApp/src/app/app.module.ts
+++ CloudCertificate/ClientApp/src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { PersonsComponent } from './persons/persons.component';
+import { NotifierModule } from 'angular-notifier';
 
 @NgModule({
   declarations: [
@@ -24,6 +24,7 @@ import { PersonsComponent } from './persons/persons.component';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    NotifierModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
