d726a2a1f0b0d7a48ecb4164cd4f7477eb1e7352
Andreas Dragoman <andreas.dragoman@ness.com>
2023-10-03 17:51:48 +0300

    model upd

--- SBShared/Models/PersonModel.cs
+++ SBShared/Models/PersonModel.cs
@@ -6,14 +6,14 @@ namespace SBShared.Models
 {
     public class PersonModel
     {
-        [Required]
-        [JsonProperty(PropertyName = "id")]
-        public Guid Id { get; set; }
         //[Required]
-        [JsonProperty(PropertyName = "firstName")]
+        //[JsonProperty(PropertyName = "id")]
+        public Guid? Id { get; set; }
+        //[Required]
+        //[JsonProperty(PropertyName = "firstName")]
         public string FirstName { get; set; }
         //[Required]
-        [JsonProperty(PropertyName = "lastName")]
+        //[JsonProperty(PropertyName = "lastName")]
         public string LastName { get; set; }
     }
 }
